98018add51947ba819fd466f95545412abaa8a97
Federico Cavallo <fc.cavallo@gmail.com>
2024-02-09 09:35:43 -0300

    Adding markets example from inj

---
+++ markets.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/InjectiveLabs/sdk-go/client/common"
+	exchangeclient "github.com/InjectiveLabs/sdk-go/client/exchange"
+	derivativeExchangePB "github.com/InjectiveLabs/sdk-go/exchange/derivative_exchange_rpc/pb"
+)
+
+func main() {
+	//network := common.LoadNetwork("mainnet", "k8s")
+	network := common.LoadNetwork("testnet", "lb")
+	exchangeClient, err := exchangeclient.NewExchangeClient(network)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	marketStatus := "active"
+	quoteDenom := "peggy0x87aB3B4C8661e07D6372361211B96ed4Dc36B1B5"
+
+	req := derivativeExchangePB.MarketsRequest{
+		MarketStatus: marketStatus,
+		QuoteDenom:   quoteDenom,
+	}
+
+	res, err := exchangeClient.GetDerivativeMarkets(ctx, &req)
+	if err != nil {
+		panic(err)
+	}
+
+	str, _ := json.MarshalIndent(res, "", " ")
+	fmt.Print(string(str))
+}

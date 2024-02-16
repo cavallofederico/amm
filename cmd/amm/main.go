6a30b07d3130e872d617b4898497227f927cb7dc
Federico Cavallo <fc.cavallo@gmail.com>
2024-02-15 23:11:49 -0300

    Adding buy order

--- cmd/amm/main.go
+++ cmd/amm/main.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	price := getPriceFromOrderbook("0x0611780ba69656949525013d947713300f56c37b6175e02f26bffa495c3208fe")
-	fmt.Println(price)
+	marketId := "0x0611780ba69656949525013d947713300f56c37b6175e02f26bffa495c3208fe"
+	price := getPriceFromOrderbook(marketId)
+	print(price)
+	conectionData, err := connect()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	createBuyOrder(marketId, price*0.99, &conectionData)
+
 }
